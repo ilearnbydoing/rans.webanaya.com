10e5608f4ed1d923e052022830238d990ec3e745
Durgesh Gupta <imdurgeshgupta@gmail.com>
2021-07-14 22:19:09 +0530

    Update form-process.php

--- form-process.php
+++ form-process.php
@@ -5,8 +5,8 @@ $phone = $_POST["phone"];
 $company = $_POST["company"];
 $city = $_POST["city"];
 
-$EmailTo = "amit@ransindia.com,durgesh@webanaya.com,info@hnhtwenty.com";
-$EmailFrom = "info@hnhtwenty.com";
+$EmailTo = "test@test.com";
+$EmailFrom = "test@test.com";
 $Subject = "Rans - You have received New Enquiry";
 
 // prepare email body text
